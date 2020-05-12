3478e8b68bb435e8001a9111f1fd9ae43bc370e0
aparus <rustamapaev@gmail.com>
2020-05-12 07:36:51 +0300

    because:secret name was converted to lower-case

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -7,7 +7,7 @@ async function dbConnect() {
 		return
 	}
 
-	const db = await mongoose.connect(process.env.MONGO_URI, {
+	const db = await mongoose.connect(process.env.mongo_uri, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
