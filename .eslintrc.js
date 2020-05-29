113f63349895915d2f633c16ec188b6dd08baea2
aparus <rustamapaev@gmail.com>
2020-05-29 18:21:35 +0300

    eslint, prettier changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,9 @@ module.exports = {
     rules: {
         "consistent-return": "off",
         "no-shadow": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-one-expression-per-line": "off",
+        "react/jsx-indent": "off",
+        "react/jsx-indent-props": "off",
     },
 }
